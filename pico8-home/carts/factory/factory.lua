8efe2ef7313bc9d70bd868b74d45a0a4a06f69d6
Patrick Calahan <github@pcal.net>
2024-03-01 15:41:51 -0800

    checkpoint

--- pico8-home/carts/factory/factory.lua
+++ pico8-home/carts/factory/factory.lua
@@ -12,7 +12,6 @@ function mapInit(address, width, height, tileWidth, tileHeight, spriteWidth, spr
   return m
 end
 
-
 function mapGet(m, x,y) 
   return peek(m.a + x + (y * m.w))
 end
@@ -34,13 +33,33 @@ end
 
 
 local map = {}
+local tiles = {}
 
 function _init()
     map = mapInit(0X4300,8,8,16,16,2,2)
-    mapSet(map,1,1,8)
-    mapSet(map,2,1,8)
-    mapSet(map,3,1,8)
+    mapSet(map,1,1,5)
+    mapSet(map,2,1,6)
+    mapSet(map,3,1,7)
     mapSet(map,4,1,8)
+
+    map.drawTile = function(m, tileNumber, sx, sy)
+      t = tiles[tileNumber]
+      if (t) spr(t.sprite, sx, sy, m.sw, m.sh, t.flipx or false, t.flipy or false)
+    end
+  
+
+    tiles[5] = { name="belt-right", sprite=64 }
+    tiles[6] = { name="belt-left",  sprite=64, flipx=true }
+    tiles[7] = { name="belt-up",    sprite=66,}
+    tiles[8] = { name="belt-down",  sprite=66, flipy=true }            
+
+
+end
+
+function flipTile(t, vert, horz) 
+  if (vert) t = t | 0b10000000
+  if (horz) t = t | 0b01000000
+  return t
 end
 
 
