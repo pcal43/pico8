bdb5f57048e4612a5c75ced6ebb9728988213f5d
Patrick Calahan <github@pcal.net>
2024-03-01 20:47:23 -0800

    checkpoint

--- pico8-home/carts/factory/factory.lua
+++ pico8-home/carts/factory/factory.lua
@@ -1,46 +1,4 @@
 
-
-function mapInit(address, width, height, tileWidth, tileHeight, spriteWidth, spriteHeight)
-  m = { a = address, w = width, h = height, tw = tileWidth, th = tileHeight, sw=spriteWidth, sh=spriteHeight  }
-  for y=0, m.h-1, 1 do  
-    for x=0, m.w-1, 1 do
-      mapSet(m, x,y, 0)
-    end
-  end
-  m.drawTile = function(m, tileNumber, sx, sy)
-    spr(tileNumber, sx, sy, m.sw, m.sh)
-  end
-  return m
-end
-
-function mapGet(m, x,y) 
-  return peek(m.a + mapOffset(m,x,y))
-end
-
-function mapSet(m, x,y, v) 
-  return poke(m.a + mapOffset(m,x,y), v)
-end
-
-function mapOffset(m, x, y)
-  return x + (y * m.w)
-end
-
-function mapCoords(m, offset)
-  return offset % m.w, flr(offset / m.w)
-end
-
-function mapDraw(m, cx, cy)
-  for my=0, m.h-1, 1 do  
-    for mx=0, m.w-1, 1 do
-      m.drawTile(m, mapGet(m, mx, my), cx, cy)
-      cx += m.tw
-    end
-    cx -= (m.tw * m.w)
-    cy += m.th
-  end
-end
-
-
 local map = {}
 local TILES = {}
 local ITEMS = {}
@@ -160,10 +118,12 @@ function _update()
   frameAlpha +=1
   if (frameAlpha == FRAMES_PER_TICK) then
 
+    local ctx = { map = map, newActors = {} }
+
+
     local actorsPerTile = {}
     local collisions = nil
 
-    local ctx = { map = map, newActors = {} }
     for a in all(actors) do
       a.mx += (a.dx or 0)
       a.my += (a.dy or 0)
@@ -202,7 +162,7 @@ function _draw()
   local m = map
   local cx = 0
   local cy = 0
-  for my=0, m.h-1, 1 do  
+  for my=0, m.h-1, 1 do 
     for mx=0, m.w-1, 1 do
       local t = TILES[mapGet(m, mx, my)]
       if t and t.sprite then
---
+++ pico8-home/carts/factory/map.lua
@@ -0,0 +1,40 @@
+function mapInit(address, width, height, tileWidth, tileHeight, spriteWidth, spriteHeight)
+    m = { a = address, w = width, h = height, tw = tileWidth, th = tileHeight, sw=spriteWidth, sh=spriteHeight  }
+    for y=0, m.h-1, 1 do  
+        for x=0, m.w-1, 1 do
+        mapSet(m, x,y, 0)
+        end
+    end
+    m.drawTile = function(m, tileNumber, sx, sy)
+        spr(tileNumber, sx, sy, m.sw, m.sh)
+    end
+    return m
+end
+  
+function mapGet(m, x,y) 
+    return peek(m.a + mapOffset(m,x,y))
+end
+  
+function mapSet(m, x,y, v) 
+    return poke(m.a + mapOffset(m,x,y), v)
+end
+  
+function mapOffset(m, x, y)
+    return x + (y * m.w)
+end
+  
+function mapCoords(m, offset)
+    return offset % m.w, flr(offset / m.w)
+end
+  
+function mapDraw(m, cx, cy)
+    for my=0, m.h-1, 1 do  
+      for mx=0, m.w-1, 1 do
+        m.drawTile(m, mapGet(m, mx, my), cx, cy)
+        cx += m.tw
+      end
+      cx -= (m.tw * m.w)
+      cy += m.th
+    end
+end
+  
